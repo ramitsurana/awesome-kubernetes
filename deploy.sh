d00e2d879679bf6016907b8258355f07dd75a9b6
ramit05 <ramit.surana@reancloud.com>
2020-06-13 00:26:11 +0530

    fix deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,12 @@
+# Deployment Script
+gem install awesome_bot
+awesome_bot docs/*.md docs/articles/*.md docs/case-studies/*.md docs/extras/*.md docs/guidelines/*.md docs/installers/*.md docs/learning-resources/*.md docs/managed-kubernetes/*.md docs/official-resources/*.md docs/projects/*.md --allow-redirect --white-list linkedin.com,blog.kcluster.io,telepresence.io,kismatic.com,slideshare.net,research.google.com,kubespray.io,twitter.com,github.com,creativecommons.org,cloud.oracle.com,amazon.com --allow-dupe
+
+
+branch1=$(git rev-parse --abbrev-ref HEAD)
+if [[ $branch1 == 'master' ]]; then
+echo "Found Master Branch"
 mkdocs gh-deploy
+else
+echo "Not found master branch."
+fi
